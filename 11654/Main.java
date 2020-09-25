77bcb5eb81ee1409b087e92fa387ab563fe4e913
Lomuhoe <46356790+lomuhoe@users.noreply.github.com>
2020-09-25 18:25:23 +0900

    Create Main.java

---
+++ 11654/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String spelling = sc.nextLine();
+        System.out.println((int)spelling.charAt(0));
+    }
+}
